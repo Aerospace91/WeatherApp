292fd5fe76a2a5a845130ef484032ead7b9a8c1f
Aerospace91 <beenters@gmail.com>
2023-12-30 00:51:15 -0800

    Logic for converting wind degrees and timestamps to human readable format

--- json_formatter.py
+++ json_formatter.py
@@ -6,13 +6,34 @@ def convert_timestamp_to_datetime(timestamp):
     datetime_obj = datetime.datetime.fromtimestamp(timestamp)
     return datetime_obj
 
+def toTextualDescription(degree):
+    if degree>337.5:
+        return 'Northerly'
+    if degree>292.5:
+        return 'North Westerly'
+    if degree>247.5:
+        return 'Westerly'
+    if degree>202.5:
+        return 'South Westerly'
+    if degree>157.5:
+        return 'Southerly'
+    if degree>122.5:
+        return 'South Easterly'
+    if degree>67.5:
+        return 'Easterly'
+    if degree>22.5:
+        return 'North Easterly'
+    return 'Northerly'
+
 json_file_path = "dump.json"
 
 with open(json_file_path) as file:
     data = json.load(file)
     
 current_data = data['current']
-    
+
+degree = current_data['wind_deg']
+
 print(f"Latitude: {data['lat']}")
 print(f"Longitude: {data['lon']}")
 print(f"Timezone: {data['timezone']}")
@@ -27,9 +48,7 @@ print(f" - Pressure: {current_data['pressure']}")
 print(f" - Humidity: {current_data['humidity']}")
 print(f" - Dew Point: {current_data['dew_point']}")
 print(f" - Uvi: {current_data['uvi']}")
-print(f" - Clouds: {current_data['clouds']}")
-print(f" - Visibility: {current_data['visibility']}")
-print(f" - Wind Speed: {current_data['wind_speed']}")
-print(f" - Wind Degree: {current_data['wind_deg']}")
+print(f" - Wind Speed: {current_data['wind_speed']} mph")
+print(f" - Wind Degree: {toTextualDescription(degree)}")
 print(f" - Weather Description: {current_data['weather'][0]['description']}")
 # print(f" - {current_data[]}")
