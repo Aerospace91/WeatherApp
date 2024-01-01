92b317a17244213adbf530806026793da12db7d2
Aerospace91 <beenters@gmail.com>
2024-01-01 09:53:59 -0800

    Figuring out QT Designer import, reference is python translation of XML import. Adding weather data to multiple tabs

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtWidgets, uic
-import sys
+import json_formatter
 
 class Ui(QtWidgets.QDialog):
     def __init__(self):
@@ -10,4 +10,53 @@ class Ui(QtWidgets.QDialog):
 
 app = QApplication([])
 window = Ui()
+window.setObjectName("Weather App")
+window.tabWidget.setTabText(0, "Current Weather")
+window.tabWidget.setTabText(1, "Daily Weather")
+
+data = json_formatter.load_json()
+current_data = data['current']
+hourly_data = data['hourly']
+daily_data = data['daily']
+degree = current_data['wind_deg']
+
+window.label.setText(f"""Latitude: {data['lat']}
+Longitude: {data['lon']}
+Timezone: {data['timezone']}
+Timezone Offset: {data['timezone_offset']}
+Current Weather:
+ - Current Date and Time: {json_formatter.convert_timestamp_to_datetime(current_data['dt'])}
+ - Sunrise Time: {json_formatter.convert_timestamp_to_datetime(current_data['sunrise'])}
+ - Sunset Time: {json_formatter.convert_timestamp_to_datetime(current_data['sunset'])}
+ - Temperature: {current_data['temp']} fahrenheit
+ - Feel Like Temperature: {current_data['feels_like']} fahrenheit
+ - Pressure: {current_data['pressure']}
+ - Humidity: {current_data['humidity']}%
+ - Dew Point: {current_data['dew_point']}
+ - Uvi: {current_data['uvi']}
+ - Wind Speed: {current_data['wind_speed']} mph
+ - Wind Direction: {json_formatter.deg_to_text(degree)}
+ - Weather Description: {current_data['weather'][0]['description']}""")
+
+daily_list = []
+for daily in daily_data:
+    daily_list.append(f"""Time: {json_formatter.convert_timestamp_to_datetime(daily['dt'])}
+     - Daily Summary: {daily['summary']}
+     - Temperature:
+     - - Day: {daily['temp']['day']}
+     - - Min Temp: {daily['temp']['min']}
+     - - Max Temp: {daily['temp']['max']}
+     - - Evening Temp: {daily['temp']['eve']}
+     - Feels like:
+     - - Day: {daily['feels_like']['day']}
+     - - Night: {daily['feels_like']['night']}
+     - - Evening: {daily['feels_like']['eve']}
+     - Humidity: {daily['humidity']}%
+     - Wind Speed: {daily['wind_speed']} mph
+     - Wind Direction: {json_formatter.deg_to_text(daily['wind_deg'])}
+     - Weather Description: {daily['weather'][0]['description']}""")
+    
+window.label_2.setText(daily_list[0])
+
+
 app.exec()
